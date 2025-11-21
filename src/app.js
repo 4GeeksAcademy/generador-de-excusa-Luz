179cc5b57d140d30621e533bb0e774815496e02a
Luz Alisson Diaz Salazar <alissondiazsalazar@gmail.com>
2025-11-21 22:05:35 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+window.onload = function(){
+  generateExcuse();
+}
+function generateExcuse(){
+  let excuse =
+  who[Math.floor(Math.random()*who.lenght)]+ " " +
+  action[Math.floor(Math.random()*action.lenght)]+ " " +
+  what[Math.floor(Math.random()*what.lenght)]+ " " +
+  when[Math.floor(Math.random()*when.lenght)];
+
+document.getElementById("excuse").innerText = excuse;
+
+}
+
